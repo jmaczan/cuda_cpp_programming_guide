6a0fe97eeb09cc7ce9679846416bb9739803d176
jmaczan <jedrzejpawel@maczan.pl>
2025-04-13 22:06:40 +0200

    Use manual memory management

--- 2.1.cu
+++ 2.1.cu
@@ -11,29 +11,43 @@ __global__ void VecAdd(float* A, float* B, float* C, int N)
 
 int main()
 {
-    float *A, *B, *C;
+    float *h_A, *h_B, *h_C;
+    float *d_A, *d_B, *d_C;
     int N = 1000000;
     size_t size = N * sizeof(float);
 
-    cudaMallocManaged(&A, size);
-    cudaMallocManaged(&B, size);
-    cudaMallocManaged(&C, size);
+    h_A = (float*)malloc(size);
+    h_B = (float*)malloc(size);
+    h_C = (float*)malloc(size);
 
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+    
     for (int i = 0; i < N; i++)
     {
-        A[i] = i;
-        B[i] = i;
+        h_A[i] = i;
+        h_B[i] = i;
     }
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-    VecAdd<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    VecAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
 
-    cudaDeviceSynchronize();
-    printf("C[1] = %f\n", C[1]);
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-    cudaFree(A);
-    cudaFree(B);
-    cudaFree(C);
+    printf("C[1] = %f\n", h_C[1]);
+
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
     return 0;
 }
